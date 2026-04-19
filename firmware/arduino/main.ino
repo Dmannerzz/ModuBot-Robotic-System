86e5c842d3367222171d98b763774aa40d3d5631
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-04-19 13:05:19 -0500

    Uploading from Arduino IDE (Compiled)

--- firmware/arduino/main.ino
+++ firmware/arduino/main.ino
@@ -1,12 +1,22 @@
 #include "motor.h"
 
+// ========== Motor Pins (REAL DEFINITIONS) ==========
+int MOTOR_IN1 = 5;
+int MOTOR_IN2 = 6;
+int MOTOR_IN3 = 7;
+int MOTOR_IN4 = 8;
+int ENA = 9;
+int ENB = 10;
+extern int currentSpeed;
+int currentSpeed = 150;
+
 void setup() {
     Serial.begin(9600);
     Serial.println("ModuBot Initializing...");
 
     moveForward();
     delay(1000);
-    stopMoving();
+    stopMotors();
 }
 
 void loop() {
