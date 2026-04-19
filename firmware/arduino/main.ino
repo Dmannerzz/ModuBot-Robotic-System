d2deb578f149497ca689a00b978589d98a210a26
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-04-19 12:36:59 -0500

    Add motor control initialization in setup

--- firmware/arduino/main.ino
+++ firmware/arduino/main.ino
@@ -1,6 +1,12 @@
+#include "motor.h"
+
 void setup() {
     Serial.begin(9600);
     Serial.println("ModuBot Initializing...");
+
+    moveForward();
+    delay(1000);
+    stopMoving();
 }
 
 void loop() {
