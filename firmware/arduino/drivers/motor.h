ddc56c1d76c0859bdc66f7e73d7965e7643f6e63
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-04-19 12:25:19 -0500

    Add motor driver header with movement functions

---
+++ firmware/arduino/drivers/motor.h
@@ -0,0 +1,12 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+void moveForward();
+void moveBackward();
+void turnLeft();
+void turnRight();
+void rotateLeft();
+void rotateRight();
+void stopMotors();
+
+#endif
