72855f22abfd72ac50b8bb63f152218de7f5f9b9
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-04-20 07:15:48 -0500

    Add motion engine function to motor.h

--- firmware/arduino/motor.h
+++ firmware/arduino/motor.h
@@ -1,6 +1,9 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
+#include <Arduino.h>
+
+// ========== BASIC MOVEMENT ==========
 void moveForward();
 void moveBackward();
 void turnLeft();
@@ -8,5 +11,8 @@ void turnRight();
 void rotateLeft();
 void rotateRight();
 void stopMotors();
+
+// ========== MOTION ENGINE (NEW) ==========
+void executeMotion(uint8_t dir);
 
 #endif
