1c256cc79b109b78b591694bca03abf307cdf48e
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-09 13:14:21 -0500

    Implement PID controller with begin and compute methods

--- firmware/esp32/src/motion/pid_controller.cpp
+++ firmware/esp32/src/motion/pid_controller.cpp
@@ -1,1 +1,25 @@
+#include "pid_controller.h"
 
+void PIDController::begin(float p, float i, float d) {
+    kp = p;
+    ki = i;
+    kd = d;
+    prevError = 0;
+    integral = 0;
+}
+
+float PIDController::compute(float target, float current) {
+    float error = target - current;
+
+    integral += error;
+    float derivative = error - prevError;
+
+    prevError = error;
+
+    return (kp * error) + (ki * integral) + (kd * derivative);
+}
+
+void PIDController::reset() {
+    prevError = 0;
+    integral = 0;
+}
