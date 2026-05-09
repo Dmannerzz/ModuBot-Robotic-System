743d050c0fa52ec7aed05bc099180421f803fa68
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-09 16:43:09 -0500

    Add PatrolSystem class for managing patrol operations

--- firmware/esp32/src/systems/patrol_system.h
+++ firmware/esp32/src/systems/patrol_system.h
@@ -1,1 +1,34 @@
+#pragma once
 
+#include <Arduino.h>
+#include "route_logger.h"
+#include "motion_engine.h"
+
+class PatrolSystem {
+public:
+
+    void begin(RouteLogger* loggerRef,
+               MotionEngine* motionRef);
+
+    void start();
+
+    void stop();
+
+    void update();
+
+    bool isRunning();
+
+private:
+
+    RouteLogger* logger = nullptr;
+
+    MotionEngine* motion = nullptr;
+
+    bool running = false;
+
+    int currentStep = 0;
+
+    unsigned long stepStartTime = 0;
+
+    void executeStep(const RouteStep& step);
+};
