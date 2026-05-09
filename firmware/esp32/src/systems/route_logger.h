206ec308a0fa220cbd9d1f0d280de29020467e47
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-09 15:52:58 -0500

    Add RouteLogger class for event logging

--- firmware/esp32/src/systems/route_logger.h
+++ firmware/esp32/src/systems/route_logger.h
@@ -1,1 +1,38 @@
+#pragma once
+#include <Arduino.h>
+#include "event_system.h"
 
+#define MAX_ROUTE_STEPS 50
+
+struct RouteStep {
+    EventType action;
+    uint32_t duration;
+};
+
+class RouteLogger {
+public:
+    void begin();
+
+    void startRecording();
+    void stopRecording();
+
+    void logEvent(EventType type);
+
+    RouteStep getStep(int index);
+    int getStepCount();
+
+    void clear();
+
+    bool isRecording();
+
+private:
+    RouteStep route[MAX_ROUTE_STEPS];
+
+    bool recording = false;
+
+    int stepCount = 0;
+
+    EventType lastAction = EventType::NONE;
+
+    uint32_t lastTimestamp = 0;
+};
