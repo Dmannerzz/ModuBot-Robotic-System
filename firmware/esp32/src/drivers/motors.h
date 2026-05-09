aa2bb71ad756b4496316eb4d0467d8d064338314
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-09 13:19:35 -0500

    Add Motors class with control methods

--- firmware/esp32/src/drivers/motors.h
+++ firmware/esp32/src/drivers/motors.h
@@ -1,1 +1,9 @@
+#pragma once
 
+class Motors {
+public:
+    static void set(int left, int right);
+    static void stop();
+    static void turnLeft(int speed);
+    static void turnRight(int speed);
+};
