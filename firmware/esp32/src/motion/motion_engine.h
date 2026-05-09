f8547743d5f20ac2703a4acd98760febc4a02a19
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-09 13:16:47 -0500

    Add MotionEngine class for motion control

--- firmware/esp32/src/motion/motion_engine.h
+++ firmware/esp32/src/motion/motion_engine.h
@@ -1,1 +1,19 @@
+#pragma once
+#include "pid_controller.h"
 
+class MotionEngine {
+public:
+    void begin();
+
+    void forward(int speed);
+    void backward(int speed);
+    void left(int speed);
+    void right(int speed);
+    void stop();
+
+    void setYaw(float yaw);
+
+private:
+    PIDController pid;
+    float targetYaw = 0;
+};
