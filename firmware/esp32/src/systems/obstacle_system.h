411f8253608cef1c4b4b34037f6a290e41e47eb9
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-09 14:02:58 -0500

    Add ObstacleSystem class with distance reading functionality

--- firmware/esp32/src/systems/obstacle_system.h
+++ firmware/esp32/src/systems/obstacle_system.h
@@ -1,1 +1,22 @@
+#pragma once
+#include "event_system.h"
 
+class ObstacleSystem {
+public:
+    void begin(EventQueue* queue);
+    void update();
+
+private:
+    EventQueue* eventQueue;
+
+    int lastDistance = 100;
+    bool obstacleState = false;
+
+    int readDistance();
+    bool isValidReading(int d);
+
+    // filtering
+    int smoothDistance(int newReading);
+    int buffer[5] = {100,100,100,100,100};
+    int index = 0;
+};
