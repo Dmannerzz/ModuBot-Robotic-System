b44b82dca0bebfd58a5e10682ee5c3b30b4827f9
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-09 13:37:10 -0500

    Add motor control methods and initialization

--- firmware/esp32/src/drivers/motors.h
+++ firmware/esp32/src/drivers/motors.h
@@ -1,9 +1,21 @@
 #pragma once
+#include <Arduino.h>
 
 class Motors {
 public:
-    static void set(int left, int right);
+    static void init();
+
+    static void setLeft(int speed);
+    static void setRight(int speed);
+
+    static void forward(int speed);
+    static void backward(int speed);
+    static void left(int speed);
+    static void right(int speed);
+
     static void stop();
-    static void turnLeft(int speed);
-    static void turnRight(int speed);
+
+private:
+    static void applyLeft(int pwm, bool forward);
+    static void applyRight(int pwm, bool forward);
 };
