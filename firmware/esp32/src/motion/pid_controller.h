42ab6e8b5ba982b6f7b7cc9715ac257fe2272738
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-09 13:11:54 -0500

    Add PIDController class for motion control

--- firmware/esp32/src/motion/pid_controller.h
+++ firmware/esp32/src/motion/pid_controller.h
@@ -1,1 +1,17 @@
+#pragma once
 
+class PIDController {
+public:
+    void begin(float kp, float ki, float kd);
+
+    float compute(float target, float current);
+    void reset();
+
+private:
+    float kp = 0;
+    float ki = 0;
+    float kd = 0;
+
+    float prevError = 0;
+    float integral = 0;
+};
