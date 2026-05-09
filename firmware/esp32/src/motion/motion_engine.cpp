31e6fd9f14f8272304ffb7b2d0ffb90322002fca
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-09 13:18:21 -0500

    Implement motion control methods in MotionEngine

--- firmware/esp32/src/motion/motion_engine.cpp
+++ firmware/esp32/src/motion/motion_engine.cpp
@@ -1,1 +1,38 @@
+#include "motion_engine.h"
+#include "motors.h"
+#include <Arduino.h>
 
+void MotionEngine::begin() {
+    pid.begin(2.0, 0.0, 0.5); // tune later
+}
+
+void MotionEngine::setYaw(float yaw) {
+    targetYaw = yaw;
+}
+
+void MotionEngine::forward(int speed) {
+    float correction = pid.compute(targetYaw, 0); 
+    int left = speed + correction;
+    int right = speed - correction;
+
+    left = constrain(left, 0, 255);
+    right = constrain(right, 0, 255);
+
+    Motors::set(left, right);
+}
+
+void MotionEngine::backward(int speed) {
+    Motors::set(-speed, -speed);
+}
+
+void MotionEngine::left(int speed) {
+    Motors::turnLeft(speed);
+}
+
+void MotionEngine::right(int speed) {
+    Motors::turnRight(speed);
+}
+
+void MotionEngine::stop() {
+    Motors::stop();
+}
