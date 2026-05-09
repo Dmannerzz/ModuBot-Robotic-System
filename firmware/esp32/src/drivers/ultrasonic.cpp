8771f23c40b749c1510b7894373dc91a8c32f4b3
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-09 18:45:13 -0500

    Implement Ultrasonic sensor driver functions

--- firmware/esp32/src/drivers/ultrasonic.cpp
+++ firmware/esp32/src/drivers/ultrasonic.cpp
@@ -1,1 +1,26 @@
+#include "ultrasonic.h"
+#include <Arduino.h>
 
+#define TRIG_PIN 26
+#define ECHO_PIN 36
+
+void Ultrasonic::begin() {
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+}
+
+int Ultrasonic::readDistance() {
+
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    long duration = pulseIn(ECHO_PIN, HIGH, 50000);
+
+    if (duration <= 0) return 0;
+
+    return duration * 0.034 / 2;
+}
