a5326b71bb4ce45ca78755872cae63c30a8a9213
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-11 12:48:14 -0500

    Add IMU class header with basic methods

--- firmware/esp32/src/drivers/imu.h
+++ firmware/esp32/src/drivers/imu.h
@@ -1,1 +1,19 @@
+#pragma once
+#include <Arduino.h>
 
+class IMU {
+public:
+    void begin();
+
+    // call frequently in loop
+    void update();
+
+    float getYaw() const;
+
+private:
+    float yaw = 0;
+
+    // simulation fallback / placeholder filter
+    float gyroZ = 0;
+    unsigned long lastTime = 0;
+};
