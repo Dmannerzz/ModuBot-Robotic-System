499e082ef3800857f66154141e7d58c3cb8d8776
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-11 13:59:31 -0500

    Refactor IMU class methods and variables

--- firmware/esp32/src/drivers/imu.h
+++ firmware/esp32/src/drivers/imu.h
@@ -1,25 +1,23 @@
 #pragma once
-
+#include <Arduino.h>
+#include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 
 class IMU {
 public:
     void begin();
-    void update();
 
-    float getYaw() const;
-    float getGyroZ() const;
+    void update();      // refresh sensor data
+    float getYaw();     // main output for robot
 
 private:
     Adafruit_MPU6050 mpu;
 
     float yaw = 0;
-    float gyroZ = 0;
+    float gyroZOffset = 0;
 
-    float offsetZ = 0;
-
-    unsigned long lastTime = 0;
+    unsigned long lastUpdate = 0;
 
     void calibrate();
 };
