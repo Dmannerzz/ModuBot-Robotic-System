d15b3c25d6435f505ff068e90a105ae611e9676e
Dmannerzz <168145105+Dmannerzz@users.noreply.github.com>
2026-05-11 13:32:55 -0500

    Define StateMachine class and RobotState enum

--- firmware/esp32/src/core/state_machine.h
+++ firmware/esp32/src/core/state_machine.h
@@ -1,9 +1,39 @@
 #pragma once
+
 #include "event_system.h"
+
+class MotionEngine;
+class RouteLogger;
+class PatrolSystem;
+class ControlPolicy;
 
 enum class RobotState {
     IDLE,
     MANUAL,
     OBSTACLE_AVOIDANCE,
     PATROL
+};
+
+class StateMachine {
+public:
+
+    void init(EventQueue* queue);
+    void update();
+
+private:
+
+    void handleEvent(const Event& event);
+
+    void handleManual(const Event& event);
+    void handleObstacle(const Event& event);
+    void handlePatrol(const Event& event);
+    void handleIdle(const Event& event);
+
+    void transitionTo(RobotState newState);
+
+private:
+
+    EventQueue* eventQueue = nullptr;
+
+    RobotState currentState = RobotState::IDLE;
 };
